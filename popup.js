ec329b293dbd4801083e140083f3bf56852d0f82
benzenetarou <benzenetarou@gmail.com>
2017-03-28 22:27:35 +0900

    make a value record storable in chrome storage

--- popup.js
+++ popup.js
@@ -1,16 +1,48 @@
-function count(){
-  var txtVal = $('textarea').val();
-  var words = txtVal.trim().replace(/\s+/gi, ' ').split(' ').length;
-  var chars = txtVal.length;
-  var time = $('.timer').val();
-  var wpm = words/(time/60);
-  if(chars===0){words=0;}
-  $('#counter').html('<br>'+'<p>'+words+' words and '+ chars +' characters'+'</p>'+'<p>'+ 'WPM:'+wpm);
+var words;
+var article;
+var remembered = false;
+var time;
+var wpm
 
+$(document).ready(function(){
+  retrieve();
+  count();
+})
+
+$('#article').on('keyup propertychange paste', function(){
+  count();
+});
+$('#timer').on('keyup propertychange paste', function(){
+  count();
+});
+
+function count(){
+  console.log("count()")
+  article = $('textarea').val();
+  words = article.trim().replace(/\s+/gi, ' ').split(' ').length;
+  time = $('#timer').val();
+  wpm = words/(time/60);
+  $('#words_result').html(words + ' words');
+  $('#wpm').html('WPM:' + wpm);
+  store();
 }
 
-count();
+function store() {
+  console.log("store")
+  chrome.storage.sync.set({
+    'article': article,
+    'words': words
+  }, function(storage) {
+  });
+}
 
-$('textarea').on('keyup propertychange paste', function(){ 
-    count();
-});
+function retrieve(){
+  chrome.storage.sync.get(['article','words'], function(items){
+    if(!jQuery.isEmptyObject(items)){
+      if(items.article != undefined){
+        $('#article').val(items.article);
+        $('#words_result').html(items.words + "words");
+      }
+    }
+  });
+}
