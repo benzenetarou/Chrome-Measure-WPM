61f0def0ae4a38b0acb7d31832b663846c44cfb8
benzenetarou <benzenetarou@gmail.com>
2017-04-14 06:31:20 +0900

    add comment, discard what is unnecessary

--- popup.js
+++ popup.js
@@ -1,18 +1,14 @@
-var words;
-var article;
-var remembered = false;
-var time;
-var wpm;
-var startTime;
-var stopTime;
+var words, article, time, wpm;
+var startTime, stopTime;
 var now;
 var running;//タイマーが動いているとき、true
-var counting;
+var counting;//カウントを毎秒行う関数
 
-$(document).ready(function() {
+$(document).ready(function() {//ポップアップを開いたときに一度だけ実行される
     retrieve(count);
-    console.log("ready: running=" + running);
-    setTimeout(function(){runtimer()},300);
+    //ポップアップを開いて、timer計測するかどうかを投げる。並行処理のために、setTimeoutを使用
+    setTimeout(function(){runtimer()},100);
+
 })
 
 $('#article').on('keyup propertychange paste', function() {
@@ -39,11 +35,9 @@ function count() {
     }
     $('#timer').val(time);
     wpm = Math.floor(words / (time / 60));
-    // startTime = $('#startTimeValue').val();
     $('#words_result').html(words + ' words');
     $('#wpm').html('WPM:' + wpm);
     store();
-    console.log("count done")
 }
 
 function store() {
@@ -54,37 +48,26 @@ function store() {
         'stopTime': stopTime,
         'running': running
     });
-    console.log(startTime);
-    console.log("store done")
 }
 
-function retrieve(callback) {
+function retrieve(callback) {//ポップアップを開いたときに、保持してある情報を戻す
     chrome.storage.local.get(['article', 'words', 'startTime', 'stopTime', 'running'], function(items) {
         if (!jQuery.isEmptyObject(items)) {
             if (items.article != undefined) {
                 $('#article').val(items.article);
                 $('#words_result').html(items.words + "words");
-                $('#startTimeValue').val(items.startTime);
-                $('#stopTimeValue').val(items.stopTime);
-
                 startTime = items.startTime;
                 stopTime = items.stopTime;
                 running = items.running;
             }
-            callback();
+            callback();//基本的には$(document).readyでcountを呼ぶだけ。
         }
     });
 }
 
 
 function startTimer() {
-    var hour, minute, second;
     startTime = new Date().getTime();
-    stopTime = "";
-    // hour = startTime.getHours();
-    // minute = startTime.getMinutes();
-    // seconds = startTime.getSeconds();
-    // $("#startTimeValue").val(hour + ":" + minute + ":" + seconds);
     $("#startTimeValue").val(startTime);
     running = true;
     runtimer();
@@ -92,17 +75,15 @@ function startTimer() {
 }
 
 function stopTimer() {
-    var hour, minute, second;
     stopTime = new Date().getTime();
     $("#stopTimeValue").val(stopTime);
-    clearInterval(counting);
     running = false;
+    clearInterval(counting);
     store();
 }
 
-function runtimer() {
+function runtimer() {//ポップアップ時にtimerを起動するかどうかを決める
     if(running){
         counting = setInterval(function(){count()},1000);
     }
-    console.log("running:" + running);
 }
